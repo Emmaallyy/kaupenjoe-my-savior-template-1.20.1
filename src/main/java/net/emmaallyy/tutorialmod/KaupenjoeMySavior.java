915bcfd33f070858188f11815094a7a9742543e7
Emmaallyy <n0xicitybusiness@gmail.com>
2024-06-24 23:18:48 -0500

    rahh

--- src/main/java/net/emmaallyy/tutorialmod/KaupenjoeMySavior.java
+++ src/main/java/net/emmaallyy/tutorialmod/KaupenjoeMySavior.java
@@ -1,5 +1,6 @@
 package net.emmaallyy.tutorialmod;
 
+import net.emmaallyy.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class KaupenjoeMySavior implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
